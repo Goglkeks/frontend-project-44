761165c55762b679a7615191bcddc27782911db0
GoglKeks <egorprotasovski2008@gmail.com>
2026-04-04 01:32:33 +0700

    исправление

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 import { question } from '../src/cli.js'
-import { even } from '../src/brain-even.js'
 
 console.log('Welcome to the Brain Games!')
 const userName = question()
-even(userName)
