af10ecbc5b865b1bcea7f3bb13fb0ce991366ddd
GoglKeks <egorprotasovski2008@gmail.com>
2026-04-10 00:39:25 +0700

    1

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,9 @@
+import js from '@eslint/js'
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
-export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  stylistic.configs.customize({
-    indent: 2,
-    quotes: 'single',
-    semi: false,
-    jsx: false,
-  }),
-
-]
-
+export default defineConfig([
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
+])
